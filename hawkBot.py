48ef1aa7c9072e463ca80ef4cc1fd9af926c01ec
Chaitanya Tyagi <chay2199@gmail.com>
2019-08-26 00:07:59 +0530

    add human support

--- hawkBot.py
+++ hawkBot.py
@@ -1,7 +1,9 @@
+import errno
 import random
 import socket
 import select
-
+import sys
+import time
 
 IP = "127.0.0.1"
 PORT = 8000
@@ -53,6 +55,40 @@ random_question = random.choice(question_response)
 def receive_message(client):
     return client.recv(1024)
 
+
+def human_support():
+    while True:
+        # Wait for user to input a message
+        message = input('Human_support > ')
+        # If message is not empty - send it
+        if message:
+            # Encode message to bytes
+            message = message.encode('utf-8')
+            client_socket.send(message)
+            # Sleep so that message is received on time
+            time.sleep(0.0005)
+
+        try:
+            # Now we want to loop over received messages (there might be more than one) and print them
+            recv_message = client_socket.recv(1024).decode('utf-8')
+            if recv_message == 'Bye':
+                break
+            print(f'{user} > {recv_message}')
+        except IOError as e:
+            # This is normal on non blocking connections - when there are no incoming data error is going to be raised
+            # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
+            # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
+            # If we got different error code - something happened
+            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                print('Reading error: {}'.format(str(e)))
+                sys.exit()
+
+            continue
+
+        except Exception as e:
+            # Any other exception - something happened, exit
+            print('Reading error: '.format(str(e)))
+            sys.exit()
 
 while True:
     # Calls Unix select() system call or Windows select() WinSock call with three parameters:
@@ -126,8 +162,10 @@ while True:
                     elif message.decode() in question:
                         client_socket.send(random_question.encode())
                     else:
-                        client_socket.send("I did not understand what you said, Please contact FinHawk "
-                                           "headquarters!".encode())
+                        client_socket.send("I did not understand what you said, "
+                                           "human support initiated!".encode())
+                        # Initiate human support
+                        human_support()
 
     for notified_socket in exception_sockets:
         # Remove from list for socket.socket()
